8875d1517a26d54f7aaf3383621ac73b3235b762
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-27 17:55:33 +0000

    m

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,25 +3,46 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
-	public static void q1() {
-		//Write question 1 code here
+	public static int[] bubbleSort(double[] nums){
+		int swaps=0;
+		int steps=0;
+		boolean swapped= true;
+		for(int i=0; i<nums.length-1 && swapped;i++){
+			swapped =false;
+			for(int j=0;j<nums.length-1-i;j++){
+				if(nums[j]>nums[j+1]){
+					swapped=true;
+					double tem =nums[j];
+					nums[j]=nums[j+1];
+					nums[j+1]=tem;
+					swaps=swaps+3;
+				}
+				steps++;
+			}
+		}
+		int[] answer= {swaps, steps};
+		return answer;
 	}
+	public static int[] selectionSort(double[] nums){
+		int swaps=0;
+		int steps=0;
+		for(int i=0; i<nums.length; i++){
+			int smallest=i;
+			for(int j=i+1;j<nums.length;j++){
+				if(nums[j]<nums[smallest]){
+					smallest=j;
+				}
+				steps++;
+			}
+				
+			double temp=nums[smallest];
+			nums[smallest]=nums[i];
+			nums[i]=temp;
+			swaps=swaps+3;
+			
+		}
+		int[] answer= {swaps, steps};
+		return answer;
 
-	public static void q2() {
-		//Write question 2 code here
 	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
