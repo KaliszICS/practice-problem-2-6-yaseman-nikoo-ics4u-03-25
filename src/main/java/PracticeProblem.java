9e22892c1d55c7c702b20330d3642b621072efef
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-30 20:54:25 +0000

    modify

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,10 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		double arr[] = {9, 7, 4, 2, 1, 0};
+		System.out.println(leastSwaps(arr));
+		
+
 
 	}
 	public static int[] bubbleSort(double[] nums){
@@ -26,11 +30,13 @@ public class PracticeProblem {
 	public static int[] selectionSort(double[] nums){
 		int swaps=0;
 		int steps=0;
-		for(int i=0; i<nums.length; i++){
+		for(int i=0; i<nums.length-1; i++){
 			int smallest=i;
 			for(int j=i+1;j<nums.length;j++){
 				if(nums[j]<nums[smallest]){
+	
 					smallest=j;
+					swaps++;
 				}
 				steps++;
 		
@@ -39,7 +45,7 @@ public class PracticeProblem {
 			double temp=nums[smallest];
 			nums[smallest]=nums[i];
 			nums[i]=temp;
-			swaps=swaps+4;
+			swaps+=3;
 			
 		}
 		int[] answer= {swaps, steps};
@@ -50,6 +56,7 @@ public class PracticeProblem {
 		int steps=0;
 		for(int i=0; i <nums.length;i++){
 			double key=nums[i];
+			swaps++;
 			int index=i-1;
 			while(index>=0 && key< nums[index]){
 				steps++;
@@ -61,6 +68,68 @@ public class PracticeProblem {
 			swaps++;
 		}
 		int[] answer={swaps, steps};
+		return answer;
+	}
+	public static String leastSwaps(double[] nums){
+		double[] nums2=nums;
+		double[] nums3=nums;
+		int swaps= 0;
+		boolean swap=true;
+		for(int i=0; i<nums.length-1 && swap;i++){
+			swap=false;
+			for(int j =0; j<nums.length-1-i;j++){
+				if (nums[j]>nums[j+1]){
+					double temp= nums[j];
+					nums[j]=nums[j+1];
+					nums[j+1]=temp;
+					swaps+=3;
+				}
+			}
+		}
+		int swaps2=0;
+		for(int x=0;x<nums2.length; x++){
+			int smallest=x;
+			swaps2++;
+			for(int y=x+1;y<nums2.length;y++){
+				if(nums2[smallest]>nums2[y]){
+					smallest=y;
+				}
+			}
+			double temp=nums2[smallest];
+			nums2[smallest]=nums2[x];
+			nums2[x]=temp;
+			swaps2+=3;
+		}
+		int swaps3=0;
+		
+		for(int h=0; h <nums3.length;h++){
+			double key=nums3[h];
+			swaps3++;
+			int index=h-1;
+			while(index>=0 && key< nums3[index]){
+				nums3[index+1]=nums3[index];
+				index--;
+				swaps3++;
+			}
+			nums3[index+1]=key;
+			swaps3++;
+		}
+		String answer;
+		if (swaps<=swaps2){
+			if(swaps<=swaps3){
+				answer= "Bubble";
+			}
+			else{
+				answer ="Insertion";
+			}
+		}
+		else if(swaps2>=swaps3){
+			answer = "Insertion";
+		}
+		else{
+			answer ="Selection";
+		}
+		System.out.println(swaps + " "+ swaps2 + " " +swaps3);
 		return answer;
 	}
 }
